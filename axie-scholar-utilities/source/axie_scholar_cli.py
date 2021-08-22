ec2ac335ade7e720c08d1621f3285dfa62594ac0
Ferran Marin <ferran.marin.llobet@gmail.com>
2021-08-22 21:07:55 +0100

    Implement Generate Secrets

--- axie-scholar-utilities/source/axie_scholar_cli.py
+++ axie-scholar-utilities/source/axie_scholar_cli.py
@@ -4,10 +4,11 @@ Usage:
     axie_scholar_payments_cli.py payout <payments_file> <secrets_file>
     axie_scholar_payments_cli.py claim <secrets_file>
     axie_scholar_payments_cli.py generate_QR <secrets_file>
-    axie_scholar_payments_cli.py generate_secrets
+    axie_scholar_payments_cli.py generate_secrets <payments_file> [<secrets_file>]
 """
 import os
 import sys
+import json
 import logging
 
 from docopt import docopt
@@ -21,6 +22,30 @@ ch = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
 ch.setFormatter(formatter)
 log.addHandler(ch)
+
+def generate_secrets_file(payments_file_path, secrets_file_path=None):
+    if not secrets_file_path:
+        secrets_file_path = "./secrets.json"
+    payments = AxiePaymentsManager.load_json(payments_file_path)
+    secrets = AxiePaymentsManager.load_json(secrets_file_path)
+    changed = False
+    for acc in payments["Scholars"]:
+        if acc["AccountAddress"] not in secrets:
+            changed = True
+            new_secret = ''
+            while new_secret == '':
+                new_secret = input(f"Please provide secret key for account {acc['Name']}."
+                                   f"({acc['AccountAddress']}): ")
+            secrets[acc["AccounttAddress"]] = new_secret
+    
+    if changed:
+        logging.info(f"Saving secrets file at {secrets_file_path}")
+        with open(secrets_file_path, "w", encoding="utf-8") as f:
+            json.dump(secrets, f, ensure_ascii=False, indent=4)
+        logging.info(f"File saved!")
+    else:
+        logging.info("Secrets file already had all needed secrets!")
+
 
 def run_cli():
     """ Wrapper function for testing purposes"""
@@ -43,7 +68,7 @@ def run_cli():
         logging.info("I shall pay my scholars!")
         apm = AxiePaymentsManager(payments_file_path, secrets_file_path)
         apm.verify_inputs()
-        
+        apm.prepare_payout() 
     elif args['claim']:
         # Claim SLP
         logging.info('I shall claim SLP')
@@ -55,7 +80,20 @@ def run_cli():
     elif args['generate_secrets']:
         # Generate Secrets
         logging.info('I shall help you generate your secrets file')
-        raise NotImplementedError('Sorry, I have yet to implement this command')
+        payments_file_path = args['<payments_file>']
+        secrets_file_path = args.get('<secrets_file>')
+        file_error = False
+        if not os.path.isfile(payments_file_path):
+            logging.critical("Please provide a correct path to the Payments file. "
+                            f"Path provided: {payments_file_path}")
+            file_error = True
+        if secrets_file_path and not os.path.isfile(secrets_file_path):
+            logging.critical("Please provide a correct path to the Secrets file. "
+                            f"Path provided: {secrets_file_path}")
+            file_error = True
+        if file_error:
+            raise Exception("Please review your file paths and re-try.")
+        generate_secrets_file(payments_file_path, secrets_file_path)
 
 
 if __name__ == '__main__':
