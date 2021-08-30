dfd5e26aa4debff4b7b404d3b360bf5591a27755
Ferran Marin <ferran.marin.llobet@gmail.com>
2021-08-30 14:06:12 +0100

    Move docker_push place

--- axie-scholar-utilities/docker/docker_push.sh
+++ axie-scholar-utilities/docker/docker_push.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/bash/env bash
+set -e # Halt script on error
+echo "Building image"
 docker-compose build scholar-utilities
+echo "Logging in to DockerHub"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "Tagging image"
 docker tag axie-scholar-utilities epith/axie-scholar-utilities
+echo "Pushing image to DockerHub"
 docker push epith/axie-scholar-utilities
