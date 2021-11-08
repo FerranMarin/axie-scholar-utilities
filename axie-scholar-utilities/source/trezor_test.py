b64522c2c9455241d8eda319333e736110e915f8
Ferran Marin <ferran.marin.llobet@gmail.com>
2021-11-08 00:47:32 +0000

    Update trezor test

--- axie-scholar-utilities/source/trezor_test.py
+++ axie-scholar-utilities/source/trezor_test.py
@@ -1,57 +1,108 @@
 import json
-import subprocess
+import rlp
 from time import sleep
-from web3 import Web3, exceptions
-from axie.utils import get_nonce
+from datetime import datetime, timedelta
 
-TARGET_ADDRESS = "0xcac6cb4a85ba1925f96abc9a302b4a34dbb8c6b0"
-ORIGIN_ADDRESS = "0x268bb18f139ee86f3bcdd46841d284e64d7a1716"
+from trezorlib.client import get_default_client
+from trezorlib.tools import parse_path
+from trezorlib import ethereum
+from web3 import Web3, exceptions
+from axie.utils import get_nonce, TIMEOUT_MINS
 
 SLP_CONTRACT = "0xa8754b9fa15fc18bb59458815510e40a12cd2014"
 RONIN_PROVIDER_FREE = "https://proxy.roninchain.com/free-gas-rpc"
 
 
-w3 = Web3(Web3.HTTPProvider(RONIN_PROVIDER_FREE))
-with open("axie/slp_abi.json") as f:
-    slb_abi = json.load(f)
-slp_contract = w3.eth.contract(
-    address=Web3.toChecksumAddress(SLP_CONTRACT),
-    abi=slb_abi
-)
-# Build transaction
-transaction = slp_contract.functions.transfer(
-    Web3.toChecksumAddress(TARGET_ADDRESS),
-    1
-).buildTransaction({
-    "chainId": 2020,
-    "gas": 100000,
-    "gasPrice": w3.toWei("0", "gwei"),
-    "nonce": get_nonce(ORIGIN_ADDRESS)
-})
-with open("transaction.json", "w") as tx_f:
-    json.dump(transaction, tx_f)
-# Trezor signature with CLI
-p = subprocess.Popen(["trezorctl", "eth", "sign-tx", "transaction.json"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = p.communicate()
-# Get signed transaction
-with open("transaction.json", "w") as signed_tx_f:
-    signed_tx = json.load(signed_tx_f)
+class TrezorPayment:
 
-w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-hash = w3.toHex(w3.keccak(signed_tx.rawTransaction))
-while True:
-    try:
-        recepit = w3.eth.get_transaction_receipt(hash)
-        if recepit["status"] == 1:
-            success = True
+    def __init__(self, name, from_acc, to_acc, amount):
+        self.w3 = Web3(Web3.HTTPProvider(RONIN_PROVIDER_FREE))
+        self.name = name
+        self.from_acc = from_acc.replace("ronin:", "0x")
+        self.to_acc = to_acc.replace("ronin:", "0x")
+        self.amount = amount
+        with open("axie/slp_abi.json", encoding='utf-8') as f:
+            slb_abi = json.load(f)
+        self.contract = self.w3.eth.contract(
+            address=Web3.toChecksumAddress(SLP_CONTRACT),
+            abi=slb_abi
+        )
+        self.client = get_default_client()
+        self.bip32_path = parse_path("m/44'/60'/0'/0/0")
+        self.address = ethereum.get_address(self.client, self.bip32_path, True)
+        self.gwei = self.w3.toWei('0', 'gwei')
+        self.gas = 250000
+
+    def execute(self):
+         # Get Nonce
+        nonce = get_nonce(self.from_acc)
+        # Build transaction
+        send_tx =  self.contract.functions.transfer(
+            Web3.toChecksumAddress(self.to_acc),
+            self.amount
+        ).buildTransaction({
+            "chainId": 2020,
+            "gas": self.gas,
+            "gasPrice": self.gwei,
+            "nonce": nonce
+        })
+
+        data = self.w3.toBytes(hexstr=send_tx['data'])
+        to = self.w3.toBytes(hexstr=SLP_CONTRACT)
+
+        sig = ethereum.sign_tx(
+            self.client,
+            n=self.bip32_path,
+            nonce=nonce,
+            gas_price=self.gwei,
+            gas_limit=self.gas,
+            to=SLP_CONTRACT,
+            value=0,
+            data=data,
+            chain_id=2020
+        )
+        transaction = rlp.encode((nonce, self.gwei, self.gas, to, 0, data) + sig)
+        # Send raw transaction
+        self.w3.eth.send_raw_transaction(transaction)
+        hash = self.w3.toHex(self.w3.keccak(transaction))
+        # Wait for transaction to finish or timeout
+        start_time = datetime.now()
+        while True:
+            # We will wait for max 10minutes for this tx to respond, if it does not, we will re-try
+            if datetime.now() - start_time > timedelta(minutes=TIMEOUT_MINS):
+                success = False
+                print(f"Transaction {self}, timed out!")
+                break
+            try:
+                recepit = self.w3.eth.get_transaction_receipt(hash)
+                if recepit["status"] == 1:
+                    success = True
+                else:
+                    success = False
+                break
+            except exceptions.TransactionNotFound:
+                # Sleep 10s while waiting
+                sleep(10)
+                print(f"Waiting for transaction '{self}' to finish (Nonce:{nonce})...")
+
+        if success:
+            print(f"Important: Transaction {self} completed! Hash: {hash} - "
+                  f"Explorer: https://explorer.roninchain.com/tx/{str(hash)}")
         else:
-            success = False
-        break
-    except exceptions.TransactionNotFound:
-        print(f"Waiting for transaction to finish (Hash:{hash})...")
-        # Sleep 5 seconds not to constantly send requests!
-        sleep(5)
-if success:
-    print("Transaction hash: {hash} - Explorer: https://explorer.roninchain.com/tx/{str(hash)}")
-else:
-    print("FAIL!")
+            print(f"Important: Transaction {self} failed.")
+
+    def __str__(self):
+        return f"{self.name}({self.to_acc.replace('0x', 'ronin:')}) for the amount of {self.amount} SLP"
+
+
+
+
+if __name__ == '__main__':
+    # this will send 1 SLP from 1 acc to another
+    t = TrezorPayment(
+        "Test payment",
+        "ronin:your_origin_account_here",
+        "ronin:your_destination_account_here",
+        1
+    )
+    t.execute()
