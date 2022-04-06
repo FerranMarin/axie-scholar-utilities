0c23a3b8ab13515328c6f146361abd27312c044d
Ferran Marin <ferran.marin.llobet@gmail.com>
2022-04-06 22:12:14 +0200

    Bump versions

--- axie-scholar-utilities/source/axie/__init__.py
+++ axie-scholar-utilities/source/axie/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '3.0.7'
+__version__ = '3.0.8'
 __all__ = [
     'AxiePaymentsManager',
     'AxieClaimsManager',
--- axie-scholar-utilities/source/trezor_axie_scholar_cli.py
+++ axie-scholar-utilities/source/trezor_axie_scholar_cli.py
@@ -175,7 +175,7 @@ def check_file(file):
 
 def run_cli():
     """ Wrapper function for testing purposes"""
-    args = docopt(__doc__, version='Trezor Axie Scholar Payments CLI v3.0.7')
+    args = docopt(__doc__, version='Trezor Axie Scholar Payments CLI v3.0.8')
     if args['payout']:
         logging.info("I shall help you pay!")
         payments_file_path = args['<payments_file>']
